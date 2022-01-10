a47083f96d949f9f7ada2dae2329926595b5c46a
Joshua Sheng <joshua.sheng@freestar.io>
2022-01-10 11:38:30 -0500

    drafted up api logic

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -3,16 +3,24 @@ package controllers
 import models.Card
 import models.Hand
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.libs.json._
 
 import javax.inject.{Inject, Singleton}
 
 @Singleton
 class ApiController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+  implicit val cardJson = Json.format[Card]
+  implicit val handJson = Json.format[Hand]
   def returnData(): Action[AnyContent] = Action {
     NoContent
   }
   def returnCards(numCards: Int) = Action {
-    val cards = for (i <- 1 to numCards) yield Card(0, 0)
-      Hand(cards.toArray)
+    if (numCards < 1 || numCards > 52) {
+      NoContent
+    }
+    else {
+      val cards = for (i <- 1 to numCards) yield Card(0, 0)
+      Ok(Json.toJson(Hand(cards.toArray)))
+    }
   }
 }
